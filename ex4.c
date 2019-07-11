7915ce49db6bfb744bfaa934f89fa6adbd8ea073
大西泰正 <otc3@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 19:20:49 +0900

    fourth revision

--- ex4.c
+++ ex4.c
@@ -2,6 +2,11 @@
 #include<stdlib.h>
 #include<time.h>
 int main(){
+    printf("What is your name?\n");
+    char name[100];
+    scanf("%s",name);
+    printf("Hello, %s\n",name);
+    printf("Rolling the dice...\n");
     int s;
     srand(time(NULL));
     int total = 0;
@@ -11,10 +16,11 @@ int main(){
         printf("Die %d: %d\n",i+1,s);
     }
     printf("Total value: %d\n",total);
-    if(total > 7){printf("You won!\n");}
-    else printf("You lost\n");
-
+    if(total > 7){printf("%s won!\n",name);}
+    else printf("%s lost\n",name);
+    
     return 0;
     
 }
+
 
